4c2448d0836689971b56064432250d1daaaedc9c
Muhammad Hannan <mdhannan.info@gmail.com>
2023-06-22 16:49:26 -0700

    change home page route

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use App\Http\Controllers\UserController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 Route::middleware('auth')->group(function () {
     Route::resource('users', UserController::class);
